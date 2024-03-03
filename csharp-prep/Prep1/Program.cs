86a8ff45b95fc618ff7159330cac3d140a0a71de
RoiJohanny <johannyjoel2@gmail.com>
2024-03-03 15:59:45 +0100

    prep 1 update

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("what is your first name:");
+        string First = Console.ReadLine();
+        Console.WriteLine("what is your last name:");
+        string Last  = Console.ReadLine();
+        Console.WriteLine($"your name is {Last}, {First} {Last}");
     }
 }
